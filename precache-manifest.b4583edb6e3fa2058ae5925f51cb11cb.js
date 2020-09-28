ac703d33034c6166e8bbe0e31502db189d2bea86
luckshim <shimpark@gmail.com>
2020-09-29 02:44:20 +0900

    Updates

---
+++ precache-manifest.b4583edb6e3fa2058ae5925f51cb11cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a45598bc6e4610283665f43a715ae8",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "45539790d6ec30da1459",
+    "url": "/movie_app_2020/static/css/main.dfcd5835.chunk.css"
+  },
+  {
+    "revision": "97be323d1c74a1087b63",
+    "url": "/movie_app_2020/static/js/2.d1bc6ec3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.d1bc6ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45539790d6ec30da1459",
+    "url": "/movie_app_2020/static/js/main.af856a79.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
