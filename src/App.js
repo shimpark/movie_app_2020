f28c3aa770db50bcc729e85bb1d338b9cfc15be8
luckshim <shimpark@gmail.com>
2020-09-28 23:47:08 +0900

    count 자동증가 감소 작업

--- src/App.js
+++ src/App.js
@@ -1,51 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, pic }){
+class App extends React.Component {
 
-  return (
-  <div>
-    <h1>i love {name}!!</h1>
-    <img src={pic} alt={name} />
-  </div>
-  )
-}
-
-const foodLike = [
-  {
-    id : 1,
-    name : 'kimchi',pic : "https://t1.daumcdn.net/news/202009/25/newsen/20200925143026721covj.jpg"    
-  },
-  {
-    id : 2,
-    name : 'bibimbo',pic : "https://img4.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202009/24/newsen/20200924152905807wefh.jpg"    
-  },{
-    id : 3,
-    name : 'bulgogi',pic : "https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202009/24/newsen/20200924152906144kjsb.jpg"        
+  state = {
+    count : 0
   }
-];
 
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  pic : PropTypes.string.isRequired,  
-  //rating : PropTypes.string.isRequired
-};
+  add = () => {
+    
+    //this.setState({count : this.state.count + 1});
 
-/*
-function renderFood(dish){
-  return <Food name={dish.name} pic={dish.pic}></Food>;
-}
-*/
+    this.setState(current => ({
+      count : current.count + 1
+    }));
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodLike.map(dish => (
-        <Food key={dish.id} name={dish.name} pic={dish.pic}></Food>
-      ))}
-    </div>    
-  );
+  minus = () => {
+    //this.setState({count : this.state.count - 1});
+
+    this.setState(current => ({
+      count : current.count - 1
+    }))
+  }
+  
+  render() {
+    return (
+        <div>
+          <h1>i am class {this.state.count}</h1>
+          <button onClick={this.add}>+1</button>
+          <button onClick={this.minus}>-1</button>
+        </div>
+      );
+  }
 }
 
 export default App;
