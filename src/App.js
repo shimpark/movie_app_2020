aca90786335cfb6023aa423a0347ff11d85f7933
luckshim <shimpark@gmail.com>
2020-09-29 00:06:35 +0900

    loading... 만들기

--- src/App.js
+++ src/App.js
@@ -2,59 +2,21 @@ import React from 'react';
 
 class App extends React.Component {
 
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
-
   state = {
-    count : 0
-  }
-
-  add = () => {
-    
-    //this.setState({count : this.state.count + 1});
-
-    this.setState(current => ({
-      count : current.count + 1
-    }));
-  }
-
-  minus = () => {
-    //this.setState({count : this.state.count - 1});
-
-    this.setState(current => ({
-      count : current.count - 1
-    }))
-  }
+    isLoading : true,
+    movie : []
+  };
 
   componentDidMount(){
-
-    //render() 함수 이후 한번 호출된다. (ready 함수과 비슷함.)
-    console.log("i am componentDidMount");
-  }
-
-  //setState 함수가 실행되면 호출되는 이벤트이다.
-  componentDidUpdate(){
-    console.log("i am componentDidUpdate");
-  }
-
-  componentWillUnmount(){
-
-    //이벤트 리스터를 삭제할 때 많이 사용합니다.
-    console.log("i am componentWillUnmout");
+    setTimeout(() => {
+      this.setState({isLoading : false});
+    }, 6000);
   }
   
   render() {
 
-    console.log("render");
-    return (
-        <div>
-          <h1>i am class {this.state.count}</h1>
-          <button onClick={this.add}>+1</button>
-          <button onClick={this.minus}>-1</button>
-        </div>
-      );
+    const { isLoading } = this.state;
+    return <div>{ isLoading ? 'loading....' : 'we are ready'}</div>
   }
 }
 
